6b77a8d264f205e57161e0bd535f095cb3428bec
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-09-09 16:29:44 +0200

    Add a simple example binary that pretty prints a given textual hex representation of a TTLV byte sequence.

---
+++ examples/hex_to_txt.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: hex_to_text <path/to/ttlv_input_hex.txt>");
+        std::process::exit(1);
+    }
+
+    let mut ttlv_hex_str = std::fs::read_to_string(&args[1]).expect("Failed to read the input file");
+
+    for string_to_remove in [" ", "\n", r#"""#, ","] {
+        ttlv_hex_str = ttlv_hex_str.replace(string_to_remove, "");
+    }
+
+    let ttlv_bin = hex::decode(ttlv_hex_str)
+        .expect("Failed to parse the input file. Make sure it is in hex format, e.g. 42007A..");
+
+    println!("{}", kmip_ttlv::to_string(&ttlv_bin));
+}
